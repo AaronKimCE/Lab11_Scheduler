2e989ee045964386900c5c7196e61ded41d61ddb
Aaron Kim <akim106@wch136-22.cs.ucr.edu>
2019-11-13 12:24:53 -0800

    Working on exercise 1

--- source/main.c
+++ source/main.c
@@ -1,7 +1,7 @@
 /*	Author: akim106
  *  Partner(s) Name: 
  *	Lab Section:
- *	Assignment: Lab #  Exercise #
+ *	Assignment: Lab 11  Exercise 1
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
@@ -12,11 +12,39 @@
 #include "simAVRHeader.h"
 #endif
 
-int main(void) {
-    /* Insert DDR and PORT initializations */
+unsigned char GetKeypadKey() {
+    PORTC = 0xEF;
+    asm("nop");
+    if (GetBit(PINC,0)==0) { return('1'); }
+    if (GetBit(PINC,1)==0) { return('4'); }
+    if (GetBit(PINC,2)==0) { return('7'); }
+    if (GetBit(PINC,3)==0) { return('*'); }
 
-    /* Insert your solution below */
+    PORTC = 0xDF;
+    asm("nop");
+    if (GetBit(PINC,0)==0) { return('2'); }
+
+    PORTC = 0xBF;
+    asm("nop");
+
+    return('\0');
+}
+
+int main(void) {
+    unsigned char x;
+    DDRB = 0xFF; PORTB = 0x00;
+    DDRC = 0xF0; PORTC = 0x0F;
     while (1) {
+      x = GetKeypadKey();
+      switch (x) {
+        case '\0': PORTB = 0x1F; break;
+        case '1': PORTB = 0x01; break;
+        case '2': PORTB = 0x02; break;
+        case '3': PORTB = 0x03; break;
+        case '4': PORTB = 0x04; break;
+        case '7': PORTB = 0x07; break;
+        case '*': PORTB = 0x0E; break;
+      }
 
     }
     return 1;

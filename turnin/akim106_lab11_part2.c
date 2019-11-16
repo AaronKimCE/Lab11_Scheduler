1a32e1fb024f682ed5cb905de8c592278187b0d0
Aaron Kim <akim106@wch136-12.cs.ucr.edu>
2019-11-15 21:21:35 -0800

    Part 2 WIP

--- turnin/akim106_lab11_part2.c
+++ turnin/akim106_lab11_part2.c
@@ -1,7 +1,7 @@
 /*	Author: akim106
  *      Partner(s) Name: 
  *	Lab Section:
- *	Assignment: Lab 11  Exercise 3
+ *	Assignment: Lab 11  Exercise 2
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
